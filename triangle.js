7ee22ae1fde381da7673e3e43d9504d3b14d2bee
sergener <sibel.ergener@gmail.com>
2016-09-26 12:29:47 -0400

    Add triangle solution

---
+++ triangle.js
@@ -0,0 +1,26 @@
+// Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+function isTriangle(a,b,c)
+{
+   return a + b > c && a + c > b && c + b > a;
+}
+
+// function isTriangle(a,b,c)
+// {
+// 	var numArr = [a,b,c]
+// 	numArr.sort(compareNumbers);
+// 	if (numArr[0] + numArr[1] > numArr[2]) {
+// 		return true;
+// 	} else {
+//    	return false};
+// }
+
+// function compareNumbers(a, b)
+// {
+//     return a - b;
+// }
+
+console.log(isTriangle(1,1,1))
+console.log(isTriangle(1,3,4))
